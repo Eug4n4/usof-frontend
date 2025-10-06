fb96909b0893388e78057a3e094261298fc31b8c
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-10-06 16:54:07 +0300

    set search bar to take most of header's place

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,23 +6,20 @@ function Header() {
     <header className="header">
       <ul className="header_list">
         <li>
-          <Link to="/">
-            <img src="/react.svg" alt="logo" />
-          </Link>
+          <div className="header_left">
+            <Link to="/">
+              <img src="/react.svg" alt="logo" />
+            </Link>
+            <Link to="/about">
+              <button type="button" className="header button">
+                About
+              </button>
+            </Link>
+          </div>
         </li>
-        <li>
-          <Link to="/about">
-            <button type="button" className="header button">
-              About
-            </button>
-          </Link>
-        </li>
-        <li>
+        <li className="search_parent">
           <div className="search_group">
             <form action="" className="search_group form">
-              <button type="submit" className="header button">
-                <i className="fa fa-search"></i>
-              </button>
               <input
                 type="text"
                 name="search"
@@ -32,45 +29,37 @@ function Header() {
             </form>
           </div>
         </li>
-        <li>
-          <Link to="/categories">
-            <button type="button" className="header button">
-              Categories
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/signin">
-            <button type="button" className="header button">
-              Sign In
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/signup">
-            <button type="button" className="header button">
-              Sign Up
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/signout">
-            <button type="button" className="header button">
-              Sign Out
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/create-post">
-            <button type="button" className="header button">
-              +
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile">
-            <img src="/vite.svg" alt="user" />
-          </Link>
+        <li className="header_right_parent">
+          <div className="header_right">
+            <Link to="/categories">
+              <button type="button" className="header button">
+                Categories
+              </button>
+            </Link>
+            <Link to="/signin">
+              <button type="button" className="header button">
+                Sign In
+              </button>
+            </Link>
+            <Link to="/signup">
+              <button type="button" className="header button">
+                Sign Up
+              </button>
+            </Link>
+            <Link to="/signout">
+              <button type="button" className="header button">
+                Sign Out
+              </button>
+            </Link>
+            <Link to="/create-post">
+              <button type="button" className="header button">
+                +
+              </button>
+            </Link>
+            <Link to="/profile">
+              <img src="/react.svg" alt="user" />
+            </Link>
+          </div>
         </li>
       </ul>
     </header>
