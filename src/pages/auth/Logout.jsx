05605fcad4280c008acd69d008bd6155ff5599aa
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-10-11 21:17:12 +0300

    logout page

---
+++ src/pages/auth/Logout.jsx
@@ -0,0 +1,28 @@
+import { useContext, useEffect, useRef } from "react";
+import AuthContext from "../../contexts/AuthContext";
+import AuthService from "../../api/services/AuthService";
+import { useNavigate } from "react-router-dom";
+
+function Logout() {
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const ignore = useRef(false);
+  useEffect(() => {
+    if (ignore.current) {
+      return;
+    }
+    ignore.current = true;
+    if (window.history.state) {
+      navigate(-1, { replace: true });
+    } else {
+      navigate("/", { replace: true });
+    }
+    if (user) {
+      AuthService.logout();
+      setUser(null);
+    }
+  }, []);
+  return <div>Logging out...</div>;
+}
+
+export default Logout;
