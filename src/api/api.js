f462c223d0b542ce7276ae81f176a267a39d2fca
Eug4n4 <Eug4n4@users.noreply.github.com>
2025-10-14 16:05:17 +0300

    update interceptor on status 401

--- src/api/api.js
+++ src/api/api.js
@@ -10,13 +10,13 @@ api.interceptors.response.use((config) => {
     return config;
 }, async (error) => {
     if (error.code === "ERR_NETWORK") {
-        return Promise.reject();
+        return Promise.reject(error?.message);
     }
     const request = error.config;
 
-    request._isRetry = true;
-    if (error.response.status >= 400 && error.response.status < 500 && error.config && !error.config._isRetry) {
+    if (error.response.status === 401) {
         try {
+            console.log("refreshing tokens")
             await api.get("auth/refresh");
             return api.request(request);
         } catch (e) {
